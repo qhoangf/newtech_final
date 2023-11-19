a7c2322fc6af4281ae82fb0501f1d880ed2f128b
Hoàng Nguyễn <87424201+qhoangf@users.noreply.github.com>
2023-11-19 20:49:41 +0700

    BE update topic

--- BE/routes/topic.js
+++ BE/routes/topic.js
@@ -4,13 +4,12 @@ let router = express.Router();
 let hashMiddlewareController = require("../controllers/hashMiddlewareController");
 let topicController = require("../controllers/topicController")
 
-// Loại API - Tên API
 router.post("/register/:id", hashMiddlewareController.verifyTokenAndStudentAuth, topicController.registerTopicFromUser);
+
 router.post("/create", hashMiddlewareController.verifyTokenAndLecturerAuth, topicController.createTopic);
+
 router.get("/", topicController.getAllTopic);
+
 router.put("/update/:id", hashMiddlewareController.verifyTokenAndLecturerAuth, topicController.updateTopic);
-
-
-
 
 module.exports = router;
