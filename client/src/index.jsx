9f16b657e66167d6e8b017750e9b4cd00fb8cd9e
Hoàng Nguyễn <87424201+qhoangf@users.noreply.github.com>
2023-12-16 04:40:28 +0700

    Update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material';
 import App from './app/App';
-import AppIndex from "./App"
-import React from "react"
-import ReactDOM from "react-dom/client"
 import * as serviceWorker from './serviceWorker';
 
 // third party style
@@ -12,19 +9,18 @@ import 'perfect-scrollbar/css/perfect-scrollbar.css';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render((window.location.pathname == "")
-  ? <>
-    <React.StrictMode>
+root.render(
+  <StyledEngineProvider injectFirst>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </>
-  : <>
-    <StyledEngineProvider injectFirst>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StyledEngineProvider>
-  </>
+    </BrowserRouter>
+  </StyledEngineProvider>
 );
 
+// for IE-11 support un-comment cssVars() and it's import in this file
+// and in MatxTheme file
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
