8653e2859a1c2e0b74a3e2f6f7dbd7e0ca681414
Hoàng Nguyễn <87424201+qhoangf@users.noreply.github.com>
2023-12-15 13:56:36 +0700

    Update FE

---
+++ client/src/components/home/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
