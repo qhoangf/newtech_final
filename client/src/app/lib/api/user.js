c49ea56c6cbbd2109d507e066e53b5270d4d7c7f
foxthehuman <119940593+kien1302@users.noreply.github.com>
2023-12-20 20:15:59 +0700

    ahihi api test

---
+++ client/src/app/lib/api/user.js
@@ -0,0 +1,9 @@
+import AxiosCLient from "@/lib/axiosClient";
+
+export async function userLogin(data) {
+  return await AxiosCLient.fetchingWithData("post", "/user/login", data);
+}
+
+export async function userRegister(data) {
+  return await AxiosCLient.fetchingWithData("post", "/user/register", data);
+}
