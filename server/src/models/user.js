eeba2497ae20ca5610ff11c89b754100458560ca
foxthehuman <119940593+kien1302@users.noreply.github.com>
2023-12-21 21:18:45 +0700

    Update user.js

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["student", "admin", "teacher"],
+      enum: ["student", "admin", "lecturer"],
       default: "student",
     },
   },
