ff2cc022caa9fc4fa6c7eedae9982199b0a706be
Hoàng Nguyễn <87424201+qhoangf@users.noreply.github.com>
2023-12-23 13:39:11 +0700

    Update FE

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Credentials", "true");
